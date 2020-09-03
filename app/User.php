88256eda349e9dea39b3ba90c067eb33a41a81a4
Onur Uslu <onrslu@gmail.com>
2020-09-03 22:42:25 +0300

    "timestamps = false" is added.

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @var bool $timestamps
+     */
+    public $timestamps = false;
 }
