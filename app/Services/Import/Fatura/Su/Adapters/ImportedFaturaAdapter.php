7e441e89a201278cb019be4ceb4539ecaf2d7e86
Onur Uslu <onrslu@gmail.com>
2021-01-31 18:59:22 +0300

    If birim_fiyat isn't assigned in ImportedFatura (Su), assigning default birim_fiyat setting is added. birim_fiyat column in imported_faturas table is made nullable.
    Took 30 minutes
    
    Took 1 minute

--- app/Services/Import/Fatura/Su/Adapters/ImportedFaturaAdapter.php
+++ app/Services/Import/Fatura/Su/Adapters/ImportedFaturaAdapter.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Import\Fatura\Su\Adapters;
 
+use App\Models\Ayar;
 use App\Services\Import\Fatura\Adapters\AbstractImportedFaturaAdapter;
 
 class ImportedFaturaAdapter extends AbstractImportedFaturaAdapter
@@ -12,6 +13,14 @@ class ImportedFaturaAdapter extends AbstractImportedFaturaAdapter
     public function getInvoicableArray()
     {
         $invoicableArray    = $this->getInvoicableArrayParent();
+
+        if (!$invoicableArray['birim_fiyat']) {
+            $invoicableArray['birim_fiyat'] = Ayar::where(
+                Ayar::COLUMN_BASLIK,
+                Ayar::FIELD_SU_TUKETIM_BIRIM_FIYAT
+            )
+                ->value(Ayar::COLUMN_DEGER);
+        }
 
         return $invoicableArray;
     }
